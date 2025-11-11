96a74d6140e26ff2c2ca3c9999356ed7b937e019
Kamin Horvath <kamin@kghorvath.com>
2025-11-11 14:48:53 -0500

    Modify 90-surface.sh to fetch kernel RPMs
    Updated the script to fetch kernel RPMs and modified the repository configuration for Fedora 42.

--- build_files/90-surface.sh
+++ build_files/90-surface.sh
@@ -9,14 +9,11 @@ for pkg in kernel kernel-core kernel-modules kernel-modules-core kernel-modules-
     rpm --erase $pkg --nodeps
 done
 
-# Fetch Common AKMODS & Kernel RPMS
-skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods:bazzite-"$(rpm -E %fedora)" dir:/tmp/akmods
-AKMODS_TARGZ=$(jq -r '.layers[].digest' </tmp/akmods/manifest.json | cut -d : -f 2)
-tar -xvzf /tmp/akmods/"$AKMODS_TARGZ" -C /tmp/
-mv /tmp/rpms/* /tmp/akmods/
-# NOTE: kernel-rpms should auto-extract into correct location
-cat /etc/dnf/dnf.conf
-cat /etc/yum.repos.d/*
+# Fetch Kernel RPMS
+mkdir /tmp/kernel-rpms
+skopeo copy --retry-times 3 docker://ghcr.io/bazzite-org/kernel-bazzite:latest-f"$(rpm -E %fedora)"-x86_64 dir:/tmp/
+KERNEL_TARGZ=$(jq -r '.layers[].digest' </tmp/kernel-rpms/manifest.json | cut -d : -f 2)
+tar -xvzf /tmp/"$KERNEL_TARGZ" -C /tmp/kernel-rpms/
 
 # Install Kernel
 dnf5 --setopt=disable_excludes=* -y install \
@@ -24,16 +21,15 @@ dnf5 --setopt=disable_excludes=* -y install \
     /tmp/kernel-rpms/kernel-core-*.rpm \
     /tmp/kernel-rpms/kernel-modules-*.rpm
 
-# TODO: Figure out why akmods cache is pulling in akmods/kernel-devel
-#dnf5 -y install \
-#    /tmp/kernel-rpms/kernel-devel-*.rpm
-
 dnf5 versionlock add kernel kernel-devel kernel-devel-matched kernel-core kernel-modules kernel-modules-core kernel-modules-extra
 
 #dnf5 -y install /tmp/akmods/kmods/*kvmfr*.rpm
 
 curl --retry 3 -Lo /etc/yum.repos.d/linux-surface.repo \
         https://pkg.surfacelinux.com/fedora/linux-surface.repo
+
+#Temp change to F42 baseurl
+sed -i 's/\$releasever/42/g' /etc/yum.repos.d/linux-surface.repo
 
 SURFACE_PACKAGES=(
     iptsd
