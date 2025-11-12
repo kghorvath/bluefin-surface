6d74db4203db207d4ac480702c91badc6bcac799
Kamin Horvath <kamin@kghorvath.com>
2025-11-12 12:10:43 -0500

    Moved editors and utilities to here
    Added installation of emacs and neovim editors, and stow and htop utilities.

--- build_files/20-utils.sh
+++ build_files/20-utils.sh
@@ -4,6 +4,11 @@ set -ouex pipefail
 
 ### Install packages
 
-# Domain Joining
-dnf5 -y install adcli freeipa-client oddjob-mkhomedir samba-common-tools samba-winbind sssd-ad sssd-ipa sssd-ldap libsss_autofs libsss_sudo sssd-nfs-idmap
+# Editors
+dnf5 -y install emacs neovim
 
+# Domain Joining
+dnf5 -y install adcli oddjob-mkhomedir samba-common-tools samba-winbind sssd-ad sssd-ipa sssd-ldap libsss_autofs libsss_sudo sssd-nfs-idmap
+
+# Utilities
+dnf5 -y install stow htop
